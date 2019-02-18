6024e354a92f658512ad67020aa1e0fbbe533b48
Marco Pegoraro <marco.pegoraro@gmail.com>
2019-02-18 16:36:27 +0100

    Add resize W modifier

--- services/stuffer/ssr/features/mod-jimp/index.js
+++ services/stuffer/ssr/features/mod-jimp/index.js
@@ -4,6 +4,7 @@ import { FEATURE_NAME } from './hooks'
 
 import resizeModifier from './resize.modifier'
 import filterModifier from './filter.modifier'
+import reizeWModifier from './resize-w.modifier'
 
 export const register = ({ settings, registerAction }) => {
     registerAction({
@@ -12,6 +13,7 @@ export const register = ({ settings, registerAction }) => {
         trace: __filename,
         handler: ({ modifiers }) => {
             modifiers.resize = resizeModifier(settings)
+            modifiers.resizeW = reizeWModifier(settings)
             modifiers.filter = filterModifier(settings)
         },
     })
